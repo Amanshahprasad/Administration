367d4ebedbef92b4d1f95e9f0b7f1411d1f1866f
Aman Shah <shahaman0011@gmail.com>
2023-05-28 00:30:53 +0530

    success

--- Administration/src/main/java/com/management/administration/service/StudentService.java
+++ Administration/src/main/java/com/management/administration/service/StudentService.java
@@ -19,44 +19,33 @@ public class StudentService {
 
     public Student getTheStudentDetailsByName(String firstName,String secondName){
         Optional<Student> optional= Optional.ofNullable(studentRepository.findByFirstNameAndLastName(firstName, secondName));
-        if(optional.isEmpty()){
 
-        }
         return optional.get();
     }
 
     public Student getTheStudentDetailsByEmail(String email){
         Optional<Student> optional= studentRepository.findById(email);
-        if(optional.isEmpty()){
-
-        }
 
         return optional.get();
     }
 
     public String updateTheDetailsOfStudent(String email,String phoneNo){
         Optional<Student> optional= studentRepository.findById(email);
-        if(!optional.isEmpty()){
+
             Student student= optional.get();
             student.setPhoneNo(phoneNo);
             studentRepository.saveAndFlush(student);
-        }
-        else{
 
-        }
 
       return "phoneNo has been updated successfully";
     }
 
     public String deleteTheDetailsOfStudent(String firstName,String secondName){
         Optional<Student> optional= Optional.ofNullable(studentRepository.findByFirstNameAndLastName(firstName, secondName));
-        if(!optional.isEmpty()){
+
             Student student= optional.get();
             studentRepository.deleteById(student.getEmail());
-        }
-        else {
 
-        }
         return "details of student has been deleted";
     }
 }
